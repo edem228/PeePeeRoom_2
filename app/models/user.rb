c5c04c8d5f418fd0e470e799f8f5abc44e7d35b9
knopfler81 <knopfler42@yahoo.fr>
2015-10-29 16:54:10 +0100

    upload securité

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
      
-
          :recoverable, :rememberable, :trackable, :validatable
 
-  geocoded_by :ip_address,
- :latitude => :lat, :longitude => :lng
-
-    after_validation :geocode
+ geocoded_by :current_sign_in_ip,
+ :latitude => :lat , :longitude => :lng
+ after_validation :geocode, if: :current_sign_in_ip_changed?
+ 
 end
 
 
